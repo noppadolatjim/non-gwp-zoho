1a9afb872c7a7ccf99881167b0fdcec2cd295162
Matong <noppadol.l@jimthompson.com>
2024-10-09 09:01:55 +0700

    Add simple query script

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,50 @@
+<!-- eslint-disable prettier/prettier -->
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    Name: {{ user?.Full_Name }} <br />
+    Vouchers: {{ displayVouchers }}
   </main>
 </template>
 <script>
 export default {
+  props: {
+    id: String,
+  },
   data() {
     return {
       count: 0,
+      user: null,
+      vouchers: [],
     };
   },
-  mounted() {
-    console.log("test");
-    ZOHO.CRM.API.getAllRecords({
-      Entity: "Leads",
-      sort_order: "asc",
-      per_page: 2,
-      page: 1,
-    }).then(function (data) {
-      console.log(data);
-    });
+  computed: {
+    displayVouchers() {
+      return this.vouchers.map((voucher) => voucher.Name).join(", ");
+    },
+  },
+  async mounted() {
+    try {
+      const userResponse = await window.ZOHO.CRM.API.searchRecord({Entity:"Contacts",Type:"criteria",Query:`JT_Membership_No:equals:${this.$props.id}`})
+      if (userResponse?.data) {
+        this.user = userResponse.data[0]
+      }
+      const voucherResponse = await window.ZOHO.CRM.API.searchRecord({
+        Entity: "Voucher",
+        Type: "criteria",
+        Query: `Contact_Name:equals:${this.user.Full_Name}`,
+      });
+      if (userResponse?.data) {
+        this.vouchers = voucherResponse.data;
+      }
+    } catch (err) {
+      console.debug(err);
+    }
   },
 };
 </script>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 import "./assets/main.css";
-window.ZOHO.embeddedApp.on("PageLoad",function () {
-  createApp(App).mount("#app");
-});
-window.ZOHO.embeddedApp.init();
 
+if (import.meta.env.DEV) {
+  createApp(App).mount("#app");
+} else {
+  window.ZOHO.embeddedApp.on("PageLoad", function (data) {
+    createApp(App, { id: data }).mount("#app");
+  });
+  window.ZOHO.embeddedApp.init();
+}
